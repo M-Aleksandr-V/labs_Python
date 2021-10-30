a99fd03c45493d42993c9454479c1f1a7d0df0cb
M-Aleksandr-V <morozov.freeze89@yandex.ru>
2021-10-30 13:39:30 +0300

    Update 10_polygons.py

--- 10_polygons.py
+++ 10_polygons.py
@@ -23,5 +23,16 @@ def draw_polygon():
         counter += 1
         line(line_length, angle)
 
-
+#while counter <= 40:
+#    counter += 1
+#    turtle.forward(step)
+#    turtle.left(90)
+#    a += 1
+#    if (a % 4) == 1:
+#        turtle.penup()
+#        x -= 15
+#        y -= 15
+#        turtle.goto(x, y)
+#        turtle.pendown()
+#        step += 30
 draw_polygon()
